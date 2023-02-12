b3c0e358cedcfeb8eaecccc6758d59b3588cc53f
DeEpRaJ <pagaredeepraj05@gmail.com>
2023-02-13 00:33:42 +0530

    try

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,13 +3,14 @@ import './homepage.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import  from "./logo.png";
+import logo from '../logo.png';
 
 export default function homepage() {
   return (
     <>
     <Navbar bg="dark" variant="dark">
         <Container>
+          <img src={logo} alt="" className='logo'/>
           <Navbar.Brand href="#home">Meow</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
@@ -19,9 +20,6 @@ export default function homepage() {
           </Nav>
         </Container>
       </Navbar>
-      <div>
-        <img src="../logo.png" alt=""></img>
-      </div>
     </>
   )
 }
