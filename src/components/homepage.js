5ad8d60b8b5c2ff53de6321687af28971b161e56
gayatri <107388386+gayatri1904@users.noreply.github.com>
2023-02-13 00:00:11 +0530

    first commit

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homepage.css';
+
+export default function homepage() {
+  return (
+    <>
+    <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#profile">profile</a>
+  <a href="#about">About</a>
+</div>
+       <div>
+        <img
+       </div>
+    </>
+  )
+}
